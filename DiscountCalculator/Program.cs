fbde223dd7a2267a067f0db34a027ef7c54abbb3
Raimy Mejia <raimym@goflowapp.com>
2020-08-24 11:15:29 -0400

    DONE

---
+++ DiscountCalculator/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace DiscountCalculator
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            string breaker;
+            do
+            {
+                CalculateDiscounts();
+
+                Console.Write("¿Desea calcular otra cantidad? (S / s = \"Sí\"; N / n = \"No\") => ");
+                breaker = Console.ReadLine();
+
+            } while (breaker.ToLower() != "n");
+
+            Console.WriteLine();
+            Console.Write("¡Sé feliz!");
+            Console.ReadKey();
+        }
+
+        private static void CalculateDiscounts()
+        {
+            Console.Clear();
+            Console.WriteLine("CALCULADORA DE DESCUENTOS");
+            Console.WriteLine();
+            Console.Write("Escriba su salario: ");
+            var amount = decimal.Parse(Console.ReadLine());
+
+            var salary = new SalaryModel(amount).CalculateDiscount();
+
+            Console.Clear();
+            Console.WriteLine("CALCULADORA DE DESCUENTOS");
+            Console.WriteLine();
+            Console.WriteLine($"Salario mensual: RD${salary.MonthlySalary:N}");
+            Console.WriteLine($"Salario anual: RD${salary.AnnualSalary:N}");
+            Console.WriteLine();
+            Console.WriteLine($"% AFP mensual: {salary.AfpPercent * 100:N}");
+            Console.WriteLine($"Descuento AFP mensual: RD${salary.AfpMonthlyDiscount:N}");
+            Console.WriteLine($"Descuento AFP anual: RD${salary.AfpAnnualDiscount:N}");
+            Console.WriteLine();
+            Console.WriteLine($"% SFS mensual: {salary.SfsPercent * 100:N}");
+            Console.WriteLine($"Descuento SFS mensual: RD${salary.SfsMonthlyDiscount:N}");
+            Console.WriteLine($"Descuento SFS anual: RD${salary.SfsAnnualDiscount:N}");
+            Console.WriteLine();
+            Console.WriteLine($"Salario mensual con descuentos AFP y SFS: RD${salary.MonthlySalaryAfterAfpAndSfsDiscount:N}");
+            Console.WriteLine($"Salario anual con descuentos AFP y SFS: RD${salary.AnnualSalaryAfterAfpAndSfsDiscount:N}");
+            Console.WriteLine();
+            Console.WriteLine("Categoría ISR:");
+            Console.WriteLine($"{salary.IsrCategory}");
+            Console.WriteLine($"Descuento ISR mensual: RD${salary.IsrMonthlyDiscount:N}");
+            Console.WriteLine($"Descuento ISR anual: RD${salary.IsrAnnualDiscount:N}");
+            Console.WriteLine();
+            Console.WriteLine($"Total descuento mensual: RD${salary.MonthlyTotalDiscount:N}");
+            Console.WriteLine($"Total descuento anual: RD${salary.AnnualTotalDiscount:N}");
+            Console.WriteLine();
+            Console.WriteLine($"Salario con descuento mensual: RD${salary.MonthlySalaryAfterDiscount:N}");
+            Console.WriteLine($"Salario con descuento anual: RD${salary.AnnualSalaryAfterDiscount:N}");
+            Console.WriteLine();
+        }
+    }
+}
---
+++ DiscountCalculator/SalaryModel.cs
@@ -0,0 +1,119 @@
+﻿namespace DiscountCalculator
+{
+    public class SalaryModel
+    {
+        public SalaryModel(decimal salary)
+        {
+            MonthlySalary = salary;
+        }
+
+        public decimal MonthlySalary { get; }
+        public decimal SfsPercent => 0.0304m;
+        public decimal AfpPercent => 0.0287m;
+        public string IsrCategory { get; private set; }
+        public decimal IsrAnnualDiscount { get; private set; }
+        public decimal IsrMonthlyDiscount { get; private set; }
+        public decimal AnnualTotalDiscount { get; private set; }
+        public decimal MonthlyTotalDiscount { get; private set; }
+        public decimal AnnualSalaryAfterDiscount { get; private set; }
+        public decimal MonthlySalaryAfterDiscount { get; private set; }
+        public decimal AnnualSalary => MonthlySalary * 12;
+        public decimal SfsMonthlyDiscount => MonthlySalary * SfsPercent;
+        public decimal SfsAnnualDiscount => SfsMonthlyDiscount * 12;
+        public decimal AfpMonthlyDiscount => MonthlySalary * AfpPercent;
+        public decimal AfpAnnualDiscount => AfpMonthlyDiscount * 12;
+        public decimal MonthlySalaryAfterAfpAndSfsDiscount => MonthlySalary - AfpMonthlyDiscount - SfsMonthlyDiscount;
+        public decimal AnnualSalaryAfterAfpAndSfsDiscount => MonthlySalaryAfterAfpAndSfsDiscount * 12;
+
+        public SalaryModel CalculateDiscount()
+        {
+            CalculateIsr();
+            SetTotalDiscount();
+            SetAmountAfterDiscount();
+
+            return this;
+        }
+
+        private void CalculateIsr()
+        {
+            const decimal FirstCategoryUpperLimitQty = 416220.00m;
+
+            if (AnnualSalaryAfterAfpAndSfsDiscount <= FirstCategoryUpperLimitQty)
+            {
+                IsrCategory = $"1. ​Rentas hasta RD${FirstCategoryUpperLimitQty}: " +
+                              "Exento";
+                IsrAnnualDiscount = 0;
+
+                return;
+            }
+
+            const decimal SecondCategoryLowerLimitQty = 416220.01m;
+            const decimal SecondCategoryUpperLimitQty = 624329.00m;
+            decimal Percent;
+
+            if (AnnualSalaryAfterAfpAndSfsDiscount >= SecondCategoryLowerLimitQty &&
+                AnnualSalaryAfterAfpAndSfsDiscount <= SecondCategoryUpperLimitQty)
+            {
+                Percent = 0.15m;
+
+                IsrCategory = $"2. ​​Rentas desde RD${SecondCategoryLowerLimitQty:N} hasta RD${SecondCategoryUpperLimitQty:N}: " +
+                              $"{Percent * 100:N0}% del excedente de RD${SecondCategoryLowerLimitQty:N}";
+
+                IsrAnnualDiscount = CalculateIsrDiscount(SecondCategoryLowerLimitQty, Percent);
+                IsrMonthlyDiscount = IsrAnnualDiscount / 12;
+                return;
+            }
+
+            const decimal ThirdCategoryLowerLimitQty = 624329.01m;
+            const decimal ThirdCategoryUpperLimitQty = 867123.00m;
+            decimal AdditionalAmount;
+
+            if (AnnualSalaryAfterAfpAndSfsDiscount >= ThirdCategoryLowerLimitQty && AnnualSalaryAfterAfpAndSfsDiscount <= ThirdCategoryUpperLimitQty)
+            {
+                AdditionalAmount = 31216.00m;
+                Percent = 0.20m;
+
+                IsrCategory = $"3. ​Rentas desde RD${ThirdCategoryLowerLimitQty:N} hasta RD${ThirdCategoryUpperLimitQty:N}: " +
+                              $"RD${AdditionalAmount:N} más el {Percent * 100:N0}% del excedente de RD${ThirdCategoryLowerLimitQty:N}";
+
+                IsrAnnualDiscount = CalculateIsrDiscount(ThirdCategoryLowerLimitQty, Percent, AdditionalAmount);
+                IsrMonthlyDiscount = IsrAnnualDiscount / 12;
+                return;
+            }
+
+            const decimal FourthCategorLowerLimitQty = 867123.01m;
+
+            if (AnnualSalaryAfterAfpAndSfsDiscount >= FourthCategorLowerLimitQty)
+            {
+                AdditionalAmount = 79776.00m;
+                Percent = 0.25m;
+
+                IsrCategory = $"4. Rentas desde  RD${FourthCategorLowerLimitQty:N} en adelante: " +
+                              $"RD${AdditionalAmount:N} más el {Percent * 100:N0}% del excedente de RD${FourthCategorLowerLimitQty:N}";
+
+                IsrAnnualDiscount = CalculateIsrDiscount(FourthCategorLowerLimitQty, Percent, AdditionalAmount);
+                IsrMonthlyDiscount = IsrAnnualDiscount / 12;
+                return;
+            }
+        }
+
+        private void SetTotalDiscount()
+        {
+            AnnualTotalDiscount = AfpAnnualDiscount + SfsAnnualDiscount + IsrAnnualDiscount;
+            MonthlyTotalDiscount = AnnualTotalDiscount / 12;
+        }
+
+        private void SetAmountAfterDiscount()
+        {
+            AnnualSalaryAfterDiscount = AnnualSalary - AnnualTotalDiscount;
+            MonthlySalaryAfterDiscount = MonthlySalary - MonthlyTotalDiscount;
+
+            var c = MonthlySalaryAfterDiscount * 12;
+        }
+
+        private decimal CalculateIsrDiscount(decimal lowerLimit, decimal percent, decimal amount = 0)
+        {
+            return ((AnnualSalaryAfterAfpAndSfsDiscount - lowerLimit) * percent) + amount;
+        }
+    }
+}
